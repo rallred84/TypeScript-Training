a8e1e2db8127a8f20200400cbd36d9d235a57d78
Robert Allred <rallred84@gmail.com>
2023-09-08 16:25:53 -0500

    Finished 02_03

--- exercise_files/Ch02/02_03b/src/demo.ts
+++ exercise_files/Ch02/02_03b/src/demo.ts
@@ -1,11 +1,13 @@
 interface Contact {
-    id: number;
-    name: string;
-    birthDate?: Date;
+  id: number;
+  name: ContactName;
+  birthDate?: Date;
 }
 
 let primaryContact: Contact = {
-    birthDate: new Date("01-01-1980"),
-    id: 12345,
-    name: "Jamie Johnson",
-}
+  birthDate: new Date("01-01-1980"),
+  id: 12345,
+  name: "Jamie Johnson",
+};
+
+type ContactName = string;
